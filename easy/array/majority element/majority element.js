eb215c74b393b03accd362361a324007b6d6bb77
Tuấn <webltrinh3@gmail.com>
2026-05-20 20:40:30 +0700

    Update majority element.js

---
+++ easy/array/majority element/majority element.js
@@ -0,0 +1,19 @@
+const majorityElement = (nums)=> {
+    let candidate;
+    let count = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+
+        if (count === 0) {
+            candidate = nums[i];
+        }
+
+        if (nums[i] === candidate) {
+            count++;
+        } else {
+            count--;
+        }
+    }
+
+    return candidate;
+};
